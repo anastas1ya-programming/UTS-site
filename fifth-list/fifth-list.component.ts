a61a5dd5982f0ea2028a87872d19548d4dae0d0a
anastas1ya-programming <106560080+anastas1ya-programming@users.noreply.github.com>
2023-08-25 22:47:58 +0300

    Beta version finished

---
+++ fifth-list/fifth-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fifth-list',
+  templateUrl: './fifth-list.component.html',
+  styleUrls: ['./fifth-list.component.scss']
+})
+export class FifthListComponent {
+
+}
---
+++ seventh-list/seventh-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seventh-list',
+  templateUrl: './seventh-list.component.html',
+  styleUrls: ['./seventh-list.component.scss']
+})
+export class SeventhListComponent {
+
+}
