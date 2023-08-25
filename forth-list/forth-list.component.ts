2a61983ab5ba842bb30a556adf520a8d2ad34499
anastas1ya-programming <106560080+anastas1ya-programming@users.noreply.github.com>
2023-08-25 22:25:35 +0300

    Add files via upload
    Third and forth lists finished

---
+++ forth-list/forth-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forth-list',
+  templateUrl: './forth-list.component.html',
+  styleUrls: ['./forth-list.component.scss']
+})
+export class ForthListComponent {
+
+}
---
+++ third-list/third-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-third-list',
+  templateUrl: './third-list.component.html',
+  styleUrls: ['./third-list.component.scss']
+})
+export class ThirdListComponent {
+
+}
